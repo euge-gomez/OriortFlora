1947774a7056391bd9152a67ef9e2c5d6e6b7370
eugeGomez <eugequimica@gmail.com>
2022-04-29 19:29:20 -0300

    Estilo a medias cursos y portfolio, fallas en funcionalidad en ambos y queda a coordianr sí los estilos están bien

--- js/app.js
+++ js/app.js
@@ -47,11 +47,26 @@ nosotros.addEventListener('click', changeTextoToNosotros);
 mision.addEventListener('click', changeTextoToMision);
 vision.addEventListener('click', changeTextoToVision);
 
+/* Funcionalidad Leer Más */
 
+let hideText_btn = document.getElementById('hideText_btn');
+let hideText = document.getElementById('hideText');
+
+hideText_btn.addEventListener('click', toggleText);
+
+function toggleText() {
+  hideText.classList.toggle('show');
+
+  if(hideText.classList.contains('show')) {
+    hideText_btn.innerHTML = 'Leer menos';
+  } else {
+    hideText_btn.innerHTML = 'Leer más';
+  }
+}
 
 
 // JS (babel) de timeline
-/* (function() {
+ (function() {
 
   // VARIABLES
   const timeline = document.querySelector(".timeline ol"),
@@ -171,4 +186,4 @@ vision.addEventListener('click', changeTextoToVision);
     });
   }
 
-})();*/
+})();
