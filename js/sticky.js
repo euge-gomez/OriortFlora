6de548a8bf66d9dcb95ea2eb8eb15c633f7629e2
Javier Zalazar <mariojavierzalazar@gmail.com>
2022-05-17 22:35:51 -0300

    agregamos btnback en el index

--- js/sticky.js
+++ js/sticky.js
@@ -1,15 +1,18 @@
 window.onscroll = function() {myFunction()};
       
-      var navbar = document.getElementById("navbar");
-      var btnWa = document.getElementById("btnWa");
-      var sticky = navbar.offsetTop;
+      const navbar = document.getElementById("navbar");
+      const btnWa = document.getElementById("btnWa");
+      const btnBack = document.getElementById("btnBack");
+      const sticky = navbar.offsetTop;
       
       function myFunction() {
         if (window.pageYOffset >= sticky) {
-          navbar.classList.add("sticky")
-          btnWa.style.top = "56px"
+          navbar.classList.add("sticky");
+          btnWa.style.top = "56px";
+          btnBack.classList.remove("d-none");
         } else {
           navbar.classList.remove("sticky");
-          btnWa.style.top = "172px"
+          btnWa.style.top = "172px";
+          btnBack.classList.add("d-none");
         }
       }
